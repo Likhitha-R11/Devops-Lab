1dadaac500889b25ccde0b65684b3abcf27651af
Likhitha-R11 <ragamlikhitha3@gmail.com>
2026-03-13 06:45:42 +0000

    Added multiple programs

---
+++ pgm2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class pgm2
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter name:");
+		String name=s.nextLine();
+		System.out.println("Name:"+name);
+	}
+}
